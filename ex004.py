4b41808d2395f20752ad7a39ee633e4cc5709325
guifernands <guiiilherme.fernandes@gmail.com>
2024-09-23 16:40:44 -0300

    ex004

--- ex004.py
+++ ex004.py
@@ -1,2 +1,7 @@
 x = input("Digite qualquer coisa: ")
 
+print("O tipo primitivo desse valor é {}".format(type(x)))
+print("O valor é um número? {}".format(x.isnumeric()))
+print("O valor é alfabético? {}".format(x.isalpha()))
+print("O valor é um espaço? {}".format(x.isspace()))
+print("O valor é alfanumérico? {}".format(x.isalnum()))
