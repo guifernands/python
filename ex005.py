7d9b3e08f1de3729ac958713e7d385084d7500b8
guifernands <guiiilherme.fernandes@gmail.com>
2025-01-11 11:38:33 -0300

    ex005 e 006

---
+++ ex005.py
@@ -0,0 +1,13 @@
+nome = 'Gui'
+sobrenome = 'Fernandes Rodrigues'
+idade = 19
+ano_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_metros = 1.77
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Altura: ', altura_metros)
---
+++ ex006.py
@@ -0,0 +1,6 @@
+nome = 'Guilherme Fernandes'
+altura = 1.77
+peso = 65
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc, '.')
