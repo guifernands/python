c07887ba72cfec3cd5635e66abaca121570db6a0
guifernands <guiiilherme.fernandes@gmail.com>
2025-01-16 15:01:37 -0300

    ex010

---
+++ ex010.py
@@ -0,0 +1,11 @@
+condicao = False
+pass_no_if = None
+
+if condicao:
+    pass_no_if = True
+    print('Passou no if')
+
+if pass_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
