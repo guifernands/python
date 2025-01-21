b535cf7d248029e99b5cf641132770c89a029e1c
guifernands <guiiilherme.fernandes@gmail.com>
2025-01-21 19:56:56 -0300

    ex014

---
+++ ex014.py
@@ -0,0 +1,13 @@
+qtd_linhas = 5
+qtd_colunas = 5
+
+linhas = 1
+
+while linhas <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print('linhas =', linhas, 'colunas =', coluna)
+        coluna += 1
+    linhas += 1
+
+print('Acabou')
