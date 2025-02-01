e8d54a37c389b8bd2f3aaa9c034eb935555bbcfb
guifernands <guiiilherme.fernandes@gmail.com>
2025-02-01 00:04:30 -0300

    ex019

---
+++ ex019.py
@@ -0,0 +1,16 @@
+chave = 'gladiador'
+palavra = ('*') * len(chave)
+
+while True:
+    tentativa = input('Digite uma letra: ')
+
+    if len(tentativa) > 1:
+        print('Digite apenas uma letra.')
+        continue
+    
+    if tentativa in chave:
+        if tentativa == g:
+            palavra = 
+    else:
+        print(palavra)
+        continue
