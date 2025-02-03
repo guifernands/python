439ab3facc71d2d5bd6013c88b54a9c1a11093a1
guifernands <guiiilherme.fernandes@gmail.com>
2025-02-03 16:01:32 -0300

    ex019 funcionando e limpo

--- ex019.py
+++ ex019.py
@@ -1,16 +1,26 @@
 chave = 'gladiador'
 palavra = ['*'] * len(chave)
+t = 0
 
 while True:
-    tentativa = input('Digite uma letra: ').lower()
 
-    if len(tentativa) > 1:
-        print('[ERRO] Digite apenas uma letra.')
-        continue
-    
-    if tentativa in chave:
-        for i, letra in enumerate(chave): # Substitui o caractere na posição correta
-            if letra in tentativa:
-                palavra[i] = tentativa
+    if '*' in palavra:
+        t+=1
+        tentativa = input('Digite uma letra: ').lower() # Letra que o usuário quer chutar
+
+        if len(tentativa) > 1:
+            print('[ERRO] Digite apenas uma letra.')
+            continue
+        
+        if tentativa in chave:
+            for i, letra in enumerate(chave): # Substitui o caractere na posição correta
+                if letra == tentativa:
+                    palavra[i] = tentativa
+        else:
+            print('Letra não encontrada.')
+
+        print(''.join(palavra)) # Mostra o progresso da palavra
+
     else:
-        print('Letra não encontrada.')
+        print('PARABÉNS!! Você acertou a palavra depois de',t ,'tentativas!')
+        break
