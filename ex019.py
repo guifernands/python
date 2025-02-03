ed9010cb83a6a45b00fc744e3509bba8c2a6991b
guifernands <guiiilherme.fernandes@gmail.com>
2025-02-03 13:30:08 -0300

    melhorando ex019

--- ex019.py
+++ ex019.py
@@ -1,16 +1,16 @@
 chave = 'gladiador'
-palavra = ('*') * len(chave)
+palavra = ['*'] * len(chave)
 
 while True:
-    tentativa = input('Digite uma letra: ')
+    tentativa = input('Digite uma letra: ').lower()
 
     if len(tentativa) > 1:
-        print('Digite apenas uma letra.')
+        print('[ERRO] Digite apenas uma letra.')
         continue
     
     if tentativa in chave:
-        if tentativa == g:
-            palavra = 
+        for i, letra in enumerate(chave): # Substitui o caractere na posição correta
+            if letra in tentativa:
+                palavra[i] = tentativa
     else:
-        print(palavra)
-        continue
+        print('Letra não encontrada.')
