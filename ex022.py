c103daa40dc747485c72cde76c76ece7917893c9
guifernands <guiiilherme.fernandes@gmail.com>
2025-02-21 14:33:25 -0300

    melhorias ex022

--- ex022.py
+++ ex022.py
@@ -1,35 +1,31 @@
 lista = []
-indices = len(lista)
 
 print('LISTA DE COMPRAS')
 while True:
-    comando = input('Selecione uma das opções [I]nserir [A]pagar [L]istar [P]arar: ')
+    comando = input('Selecione uma das opções [I]nserir [A]pagar [L]istar [P]arar: ').strip()
 
     if comando in ('I', 'i'):
         lista.append(input('Digite: '))
-        continue
 
     elif comando in ('A', 'a'):
         if not lista:
             print('[ERRO] A lista está vazia.')
             continue
+
         try:
-            indice = int(input('Digite o índice do item que deseja apagar: '))
+            indice = int(input('Digite o índice do item que deseja apagar: ').strip())
             if 0 <= indice and indice < len(lista):
                 del lista[indice]
 
             else:
                 print('[ERRO] Digite um índice válido.')
-                continue
 
         except ValueError:
             print("[ERRO] Digite um número válido.")
-            continue
         
     elif comando in ('L', 'l'):
         for indice, item in enumerate(lista):
-            print(indice, item)
-        continue    
+            print(indice, item) 
 
     elif comando in ('P', 'p'):
         break
