189400eb199542aaeaeeba1690a49c586cfdfc5c
guifernands <guiiilherme.fernandes@gmail.com>
2025-02-21 04:45:54 -0300

    ex022 lista de compras

--- ex022.py
+++ ex022.py
@@ -1,11 +1,39 @@
-inserir = ''
-apagar = ''
-listar = ''
 lista = []
+indices = len(lista)
 
-comando = input('LISTA DE COMPRAS \n Selecione uma das opções [I]nserir [A]pagar [L]istar: ').isupper()
+print('LISTA DE COMPRAS')
+while True:
+    comando = input('Selecione uma das opções [I]nserir [A]pagar [L]istar [P]arar: ')
 
-if comando == 'I':
-    lista.append(input('Digite: '))
-    
-print(lista)
+    if comando in ('I', 'i'):
+        lista.append(input('Digite: '))
+        continue
+
+    elif comando in ('A', 'a'):
+        if not lista:
+            print('[ERRO] A lista está vazia.')
+            continue
+        try:
+            indice = int(input('Digite o índice do item que deseja apagar: '))
+            if 0 <= indice and indice < len(lista):
+                del lista[indice]
+
+            else:
+                print('[ERRO] Digite um índice válido.')
+                continue
+
+        except ValueError:
+            print("[ERRO] Digite um número válido.")
+            continue
+        
+    elif comando in ('L', 'l'):
+        for indice, item in enumerate(lista):
+            print(indice, item)
+        continue    
+
+    elif comando in ('P', 'p'):
+        break
+
+    else:
+        print('[ERRO] Digite um caracter válido.')
+        continue
